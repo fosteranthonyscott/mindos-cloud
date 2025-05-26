092a1c0a3c6541d02d7cc9b95ce4952b7019995d
fosteranthonyscott <fosteranthonyscott@gmail.com>
2025-05-25 19:57:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Request logging
 app.use((req, res, next) => {
